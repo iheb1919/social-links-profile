f191098834568c41168509d7ab641c325b75c6a7
iheb1919 <ihebmejri14@gmail.com>
2024-05-20 14:10:03 +0100

    fix  btn hover

--- src/App.tsx
+++ src/App.tsx
@@ -32,10 +32,10 @@ export const listBtn = [
 ]
 export const ProfileDetails = () =>{
   return(
-    <div className='flex justify-center items-center flex-col gap-2 '>
+    <div className='flex justify-center items-center flex-col gap-0 '>
           <h1 className='text-white font-InterBold text-[1.8rem] text-center'> Jessica Randall</h1>
           <h3 className='text-Green font-InterBold text-[1.1rem] text-center'> London, United Kingdom</h3>
-          <span className='text-white text-[1rem] text-center font-InterRegular' > "Front-end developer and avid reader."</span>
+          <span className='text-white mt-7 text-[1rem] text-center font-InterRegular' > "Front-end developer and avid reader."</span>
         </div>
   )
 }
