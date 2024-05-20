4d7a6f8136f680b9d25bc3d9fbae15888e4c2782
iheb1919 <ihebmejri14@gmail.com>
2024-05-20 14:00:44 +0100

    add  btn hover

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,7 @@ return(
 }
 export const Button = ({title}:{title:string}) =>{
   return(
-    <button className='py-3 rounded-lg font-InterBold text-[1rem] text-White w-full h-fit bg-Grey'>
+    <button className='py-3 rounded-lg font-InterBold text-[1rem] text-White w-full h-fit bg-Grey hover:bg-Greenhover:text-black transition-all duration-300 '>
           {title}
     </button>
   )
